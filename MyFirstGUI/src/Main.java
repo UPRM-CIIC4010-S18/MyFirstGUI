05cc4ea1ab5baf0c0f838ea66be4278c08892a4d
bvelez <Bienvenido.Velez@upr.edu>
2018-03-15 17:41:51 -0400

    Initial commit

---
+++ MyFirstGUI/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame mainFrame = new JFrame();
+		
+		mainFrame.setSize(800, 600);
+		
+		mainFrame.setTitle("My First GUI!!!");
+		
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainFrame.setVisible(true);
+	}
+	
+}
