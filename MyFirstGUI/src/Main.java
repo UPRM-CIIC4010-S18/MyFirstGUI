ef1b8b0c965c6ff0e673a6eea8e5c3af87aaabd7
bvelez <Bienvenido.Velez@upr.edu>
2018-03-27 17:23:43 -0400

    Added car array for car race.

--- MyFirstGUI/src/Main.java
+++ MyFirstGUI/src/Main.java
@@ -1,29 +1,32 @@
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		JFrame mainFrame = new JFrame();
-		
+
 		mainFrame.setSize(800, 600);
-		
+
 		mainFrame.setTitle("My First GUI!!!");
-		
-		MyComponent carComponent = new MyComponent();
-		
-		mainFrame.add(carComponent);
-		
+
+		RaceStage raceStage = new RaceStage();
+
+		mainFrame.add(raceStage);
+
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		mainFrame.setVisible(true);
-		
+
 		while(true) {
+
 			mainFrame.repaint();
-			
+
 			Thread.sleep(25);
-			
+
 		}
+
 	}
-	
+
 }
--- MyFirstGUI/src/RaceStage.java
+++ MyFirstGUI/src/RaceStage.java
@@ -7,22 +7,31 @@ import java.util.Random;
 
 import javax.swing.JComponent;
 
-public class MyComponent extends JComponent {
+public class RaceStage extends JComponent {
 
 	private Random rand;
 
 	private Car topLeftCar;
+	
+	private Car[] cars;
 	private Car bottomRightCar;
 	private Car randomCar;	
+	private int numCars = 8;
 
-	public MyComponent() {
+	public RaceStage() {
 		rand = new Random();
 		topLeftCar = new Car(0,0);
+		cars = new Car[numCars];
+		int nextY = 0;
+		for(int i=0; i<numCars; i++) {
+			cars[i] = new Car(0, nextY);
+			nextY += 40;
+		}
 		//randomCar = new Car(rand.nextInt(this.getWidth()-60), rand.nextInt(this.getHeight()-30));	
 	}
 	
 	public boolean reachedRightEdge(Car c) {
-		return(c.getxPos()+60 > this.getWidth());
+		return(c.getxPos()+60 >= this.getWidth());
 	}
 	
 	public boolean reachedLeftEdge(Car c) {
@@ -36,12 +45,16 @@ public class MyComponent extends JComponent {
 		if (this.reachedLeftEdge(topLeftCar)) {
 			topLeftCar.setDirection(1);
 		}
-		int pixelsToMove = rand.nextInt(10);
+		int pixelsToMove = 5;
 		topLeftCar.translate(pixelsToMove*topLeftCar.getDirection(), 0);
 		
 		randomCar = new Car(rand.nextInt(this.getWidth()-60), rand.nextInt(this.getHeight()-30));
 		bottomRightCar = new Car(this.getWidth()-60, this.getHeight()-30);
 
+		for (int i=0; i<numCars; i++) {
+			cars[i].draw(g);
+		}
+		
 		//randomCar.draw(g);
 		topLeftCar.draw(g);
 		bottomRightCar.draw(g);
